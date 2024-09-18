ac60e4839662a170a4cca8b823fae0f58d0c5113
UO294177 <UO294177@EII-L04-15.aulasuo.uniovi.es>
2024-09-18 12:05:14 +0200

    new

--- src/main/java/giis/demo/jdbc/Ignacio.java
+++ src/main/java/giis/demo/jdbc/Ignacio.java
@@ -1,17 +1,20 @@
 package giis.demo.jdbc;
 
 public class Ignacio {
-	
+
 	// VERSION 1
 	private void Nothing() {
 		while(true) {
-			
 		}
-		
 	}
-	
+
 	// VERSION 1.1
 	private void Nothing2() {
 		for(;;) {}
+	}
+
+	// VERSION 1.3
+	private void Nothing3() {
+		for(int i=0;i<(int)Double.POSITIVE_INFINITY;i++) {}
 	}
 }
