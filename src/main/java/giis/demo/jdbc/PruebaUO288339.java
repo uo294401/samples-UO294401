6c15d3872d0736999edde8e83a8abc2060527761
uo288339 <uo288339@EII-L04-16.aulasuo.uniovi.es>
2024-09-18 12:03:59 +0200

    Modificacion en la clae PruebaIO288339

--- src/main/java/giis/demo/jdbc/PruebaUO288339.java
+++ src/main/java/giis/demo/jdbc/PruebaUO288339.java
@@ -3,6 +3,15 @@ package giis.demo.jdbc;
 public class PruebaUO288339 {
 
 	public static void main(String[] args) {
-		System.out.println("Prueba IPS, rama github");
+		int result = doNothing();
+		System.out.println("Prueba IPS, rama github " + result);
+	}
+	
+	public static int doNothing() {
+		int count = 0;
+		for(int i = 0; i < 100000; i++) {
+			count +=1;
+		}
+		return count;
 	}
 }
