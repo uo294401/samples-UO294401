6e0f5bc63c7b11fcac45e4fa3fc7c81c6f32675f
UO295528 <UO295528@EII-L04-13.aulasuo.uniovi.es>
2024-09-18 12:17:59 +0200

    Merge branch 'main' of https://github.com/uo294401/samples-UO294401.git into alejandro

--- src/main/java/giis/demo/tkrun/ApuestaEntity.java
+++ src/main/java/giis/demo/tkrun/ApuestaEntity.java
@@ -1,5 +1,19 @@
 package giis.demo.tkrun;
 
+/**
+ * 
+ * @author UO295528
+ * Apuesta class for tkrun package
+ */
 public class ApuestaEntity {
+	private String id; 
+	private String fecha; 
+	private double premio;
 
+	public String getId() { return this.id; }
+	public String getFecha() { return this.fecha; }
+	public double getPremio() { return this.premio; }
+	public void setId(String value) { this.id=value; }
+	public void setFecha(String value) { this.fecha=value; }
+	public void setPremio(double value) { this.premio=premio; }
 }
